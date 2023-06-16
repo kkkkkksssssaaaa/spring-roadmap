4802e9ab0698bd19d4cce1636feccc4046d5b8ab
kkkkkksssssaaaa <kofc312@naver.com>
2023-06-17 00:08:45 +0900

    feat: add member controller bean

---
+++ src/main/java/dev/kkkkkksssssaaaa/springroadmap/member/controller/MemberController.java
@@ -0,0 +1,16 @@
+package dev.kkkkkksssssaaaa.springroadmap.member.controller;
+
+import dev.kkkkkksssssaaaa.springroadmap.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
