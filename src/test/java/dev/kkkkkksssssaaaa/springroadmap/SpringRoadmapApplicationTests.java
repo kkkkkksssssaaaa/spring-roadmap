48ee058cf74a3e8517a945d12499a94e426e341b
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-17 23:05:00 +0900

    refactor: remove spring boot test annotation

--- src/test/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplicationTests.java
+++ src/test/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplicationTests.java
@@ -1,13 +1,5 @@
 package dev.kkkkkksssssaaaa.springroadmap;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class SpringRoadmapApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
