8f4bbbe7e801bb93a07f560faf7ae7edae7882f8
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-17 23:02:27 +0900

    refactor: clear main method

--- src/main/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplication.java
+++ src/main/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplication.java
@@ -1,13 +1,7 @@
 package dev.kkkkkksssssaaaa.springroadmap;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SpringRoadmapApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRoadmapApplication.class, args);
     }
-
 }
