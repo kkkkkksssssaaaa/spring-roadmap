b36a444572f8ef55987704684d91559dbbaf3798
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-20 22:02:25 +0900

    refactor: extract instance to method

--- src/main/java/dev/kkkkkksssssaaaa/springroadmap/AppConfig.java
+++ src/main/java/dev/kkkkkksssssaaaa/springroadmap/AppConfig.java
@@ -10,13 +10,21 @@ import dev.kkkkkksssssaaaa.springroadmap.domain.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(repository());
+    }
+
+    public MemoryMemberRepository repository() {
+        return new MemoryMemberRepository();
+    }
+
+    public FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-            new MemoryMemberRepository(),
-            new FixDiscountPolicy()
+            repository(),
+            discountPolicy()
         );
     }
 }
