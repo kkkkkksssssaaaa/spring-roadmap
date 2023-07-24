f3627ce864961ff26dc54d9492ff0ad9aa63a210
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-24 22:00:19 +0900

    test: refactor use to nested class

--- src/test/java/dev/kkkkkksssssaaaa/springroadmap/domain/AppTest.java
+++ src/test/java/dev/kkkkkksssssaaaa/springroadmap/domain/AppTest.java
@@ -4,6 +4,7 @@ import dev.kkkkkksssssaaaa.springroadmap.AppConfig;
 import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberService;
 import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -20,60 +21,68 @@ class AppTest {
         context = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 
-    @Test
-    void 모든_빈_출력하기() {
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+    @Nested
+    class FindBeanTest {
 
-        for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = context.getBean(beanDefinitionName);
-            System.out.println("been = " + bean);
-        }
-    }
+        @Test
+        void 모든_빈_출력하기() {
+            String[] beanDefinitionNames = context.getBeanDefinitionNames();
 
-    @Test
-    void 애플리케이션_빈_출력하기() {
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-
-        for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
-
-            /**
-             * BeanDefinition.ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
-             * BeanDefinition.ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
-             * */
-            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+            for (String beanDefinitionName : beanDefinitionNames) {
                 Object bean = context.getBean(beanDefinitionName);
+                System.out.println("been = " + bean);
+            }
+        }
 
-                System.out.println("bean = " + bean);
+        @Test
+        void 애플리케이션_빈_출력하기() {
+            String[] beanDefinitionNames = context.getBeanDefinitionNames();
+
+            for (String beanDefinitionName : beanDefinitionNames) {
+                BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
+
+                /**
+                 * BeanDefinition.ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+                 * BeanDefinition.ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+                 * */
+                if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                    Object bean = context.getBean(beanDefinitionName);
+
+                    System.out.println("bean = " + bean);
+                }
             }
         }
     }
 
-    @Test
-    void 빈_이름으로_조회() {
-        MemberService memberService = context.getBean("memberService", MemberService.class);
+    @Nested
+    class FindBeanByNameTest {
 
-        assertInstanceOf(MemberServiceImpl.class, memberService);
-    }
+        @Test
+        void 빈_이름으로_조회() {
+            MemberService memberService = context.getBean("memberService", MemberService.class);
 
-    @Test
-    void 이름_없이_타입으로만_조회() {
-        MemberService memberService = context.getBean(MemberService.class);
+            assertInstanceOf(MemberServiceImpl.class, memberService);
+        }
 
-        assertInstanceOf(MemberServiceImpl.class, memberService);
-    }
+        @Test
+        void 이름_없이_타입으로만_조회() {
+            MemberService memberService = context.getBean(MemberService.class);
 
-    @Test
-    void 구체_타입으로_조회() {
-        MemberService memberService = context.getBean("memberService", MemberServiceImpl.class);
+            assertInstanceOf(MemberServiceImpl.class, memberService);
+        }
 
-        assertInstanceOf(MemberServiceImpl.class, memberService);
-    }
+        @Test
+        void 구체_타입으로_조회() {
+            MemberService memberService = context.getBean("memberService", MemberServiceImpl.class);
 
-    @Test
-    void 빈_이름으로_조회_불가() {
-        assertThrows(
-            NoSuchBeanDefinitionException.class,
-            () -> context.getBean("xxx", MemberServiceImpl.class));
+            assertInstanceOf(MemberServiceImpl.class, memberService);
+        }
+
+        @Test
+        void 빈_이름으로_조회_불가() {
+            assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> context.getBean("xxx", MemberServiceImpl.class));
+        }
     }
 }
