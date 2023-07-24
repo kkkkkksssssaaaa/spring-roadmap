762f3b0c6f94ca976abaa745ad8d2ca3b5ebcc28
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-24 21:21:55 +0900

    test: add find bean by name test

--- src/test/java/dev/kkkkkksssssaaaa/springroadmap/domain/AppTest.java
+++ src/test/java/dev/kkkkkksssssaaaa/springroadmap/domain/AppTest.java
@@ -1,7 +1,11 @@
 package dev.kkkkkksssssaaaa.springroadmap.domain;
 
 import dev.kkkkkksssssaaaa.springroadmap.AppConfig;
+import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberService;
+import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,7 +13,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext context;
+
+    @BeforeEach
+    void beforeEach() {
+        context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
 
     @Test
     void 모든_빈_출력하기() {
@@ -38,5 +47,33 @@ class AppTest {
                 System.out.println("bean = " + bean);
             }
         }
+    }
+
+    @Test
+    void 빈_이름으로_조회() {
+        MemberService memberService = context.getBean("memberService", MemberService.class);
+
+        assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+    @Test
+    void 이름_없이_타입으로만_조회() {
+        MemberService memberService = context.getBean(MemberService.class);
+
+        assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+    @Test
+    void 구체_타입으로_조회() {
+        MemberService memberService = context.getBean("memberService", MemberServiceImpl.class);
+
+        assertInstanceOf(MemberServiceImpl.class, memberService);
+    }
+
+    @Test
+    void 빈_이름으로_조회_불가() {
+        assertThrows(
+            NoSuchBeanDefinitionException.class,
+            () -> context.getBean("xxx", MemberServiceImpl.class));
     }
 }
