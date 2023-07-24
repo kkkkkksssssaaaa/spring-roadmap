f73dab9e62a8ef9fa59ab0ec7966c04f6b9b174a
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-24 22:10:03 +0900

    test: add test when bean type is duplicated

--- src/test/java/dev/kkkkkksssssaaaa/springroadmap/domain/AppTest.java
+++ src/test/java/dev/kkkkkksssssaaaa/springroadmap/domain/AppTest.java
@@ -1,14 +1,22 @@
 package dev.kkkkkksssssaaaa.springroadmap.domain;
 
 import dev.kkkkkksssssaaaa.springroadmap.AppConfig;
+import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberRepository;
 import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberService;
 import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberServiceImpl;
+import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemoryMemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -82,7 +90,67 @@ class AppTest {
         void 빈_이름으로_조회_불가() {
             assertThrows(
                 NoSuchBeanDefinitionException.class,
-                () -> context.getBean("xxx", MemberServiceImpl.class));
+                () -> context.getBean("xxx", MemberServiceImpl.class)
+            );
+        }
+    }
+
+    @Nested
+    class DuplicatedBeanTypeTest {
+
+        @Test
+        void 타입으로_조회시_같은_타입이_둘_이상_있으면_중복_오류가_발생한다() {
+            ApplicationContext duplicateBeanApplicationContext =
+                new AnnotationConfigApplicationContext(DuplicateBeanConfig.class);
+
+            assertThrows(
+                NoUniqueBeanDefinitionException.class,
+                () -> duplicateBeanApplicationContext.getBean(MemberRepository.class)
+            );
+        }
+
+        @Test
+        void 타입으로_조회시_타입이_둘_이상_있으면_빈_이름을_명시한다() {
+            ApplicationContext duplicateBeanApplicationContext =
+                new AnnotationConfigApplicationContext(DuplicateBeanConfig.class);
+
+            assertInstanceOf(
+                MemberRepository.class,
+                duplicateBeanApplicationContext.getBean(
+                    "memberRepository1",
+                    MemberRepository.class));
+        }
+        
+        @Test
+        void 특정_타입을_모두_조회하기() {
+            ApplicationContext duplicateBeanApplicationContext =
+                new AnnotationConfigApplicationContext(DuplicateBeanConfig.class);
+
+            Map<String, MemberRepository> beansOfType 
+                = duplicateBeanApplicationContext.getBeansOfType(MemberRepository.class);
+
+            for (String key : beansOfType.keySet()) {
+                System.out.println("key = " + key);
+                System.out.println("value = " + beansOfType.get(key));
+            }
+
+            System.out.println("beansOfType = " + beansOfType);
+
+            assertEquals(2, beansOfType.size());
+        }
+
+        @Configuration
+        static class DuplicateBeanConfig {
+
+            @Bean
+            public MemberRepository memberRepository1() {
+                return new MemoryMemberRepository();
+            }
+
+            @Bean
+            public MemberRepository memberRepository2() {
+                return new MemoryMemberRepository();
+            }
         }
     }
 }
