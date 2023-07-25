8d682c8193749959e1c5c698ba1c256dd67fbb54
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-25 22:36:14 +0900

    test: add singleton object test of spring container

--- src/test/java/dev/kkkkkksssssaaaa/springroadmap/core/SingletonTest.java
+++ src/test/java/dev/kkkkkksssssaaaa/springroadmap/core/SingletonTest.java
@@ -1,6 +1,10 @@
 package dev.kkkkkksssssaaaa.springroadmap.core;
 
+import dev.kkkkkksssssaaaa.springroadmap.AppConfig;
+import dev.kkkkkksssssaaaa.springroadmap.domain.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -11,6 +15,20 @@ public class SingletonTest {
     void 싱글톤_패턴을_적용한_객체_사용() {
         SingletonService service1 = SingletonService.getInstance();
         SingletonService service2 = SingletonService.getInstance();
+
+        assertEquals(service1, service2);
+        assertSame(service1, service2);
+    }
+
+    @Test
+    void 스프링_컨테이너와_싱글톤() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService service1 = context.getBean("memberService", MemberService.class);
+        MemberService service2 = context.getBean("memberService", MemberService.class);
+
+        System.out.println("service1 = " + service1);
+        System.out.println("service2 = " + service2);
 
         assertEquals(service1, service2);
         assertSame(service1, service2);
