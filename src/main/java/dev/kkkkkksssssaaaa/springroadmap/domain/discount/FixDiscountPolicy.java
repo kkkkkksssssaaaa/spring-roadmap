4e95569b4811146b11f52b96dd4bdcf89f9a0873
kkkkkksssssaaaa <kofc312@naver.com>
2023-07-29 22:31:41 +0900

    feat: add qualifier, primary annotation to discount policies

--- src/main/java/dev/kkkkkksssssaaaa/springroadmap/domain/discount/FixDiscountPolicy.java
+++ src/main/java/dev/kkkkkksssssaaaa/springroadmap/domain/discount/FixDiscountPolicy.java
@@ -2,7 +2,11 @@ package dev.kkkkkksssssaaaa.springroadmap.domain.discount;
 
 import dev.kkkkkksssssaaaa.springroadmap.domain.member.Grade;
 import dev.kkkkkksssssaaaa.springroadmap.domain.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
