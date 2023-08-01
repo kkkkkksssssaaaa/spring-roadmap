f12f1c0f2ef3d7e7237313023d51cabf7d197ce9
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-01 22:39:24 +0900

    fix: put system argument

--- src/main/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplication.java
+++ src/main/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringRoadmapApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRoadmapApplication.class);
+        SpringApplication.run(SpringRoadmapApplication.class, args);
     }
 }
