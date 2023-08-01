e4c38da6e3e264795f259c130dba2c38f2f5d72f
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-01 22:17:37 +0900

    feat: add spring boot application annotation

--- src/main/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplication.java
+++ src/main/java/dev/kkkkkksssssaaaa/springroadmap/SpringRoadmapApplication.java
@@ -1,7 +1,12 @@
 package dev.kkkkkksssssaaaa.springroadmap;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringRoadmapApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(SpringRoadmapApplication.class);
     }
 }
