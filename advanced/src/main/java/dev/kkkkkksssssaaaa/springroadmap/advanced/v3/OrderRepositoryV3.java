b63db37238b00237a0ad76688a3db937a6207acb
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-05 17:31:57 +0900

    feat: add thread sleep at repository save

--- advanced/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advanced/v3/OrderRepositoryV3.java
+++ advanced/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advanced/v3/OrderRepositoryV3.java
@@ -21,10 +21,21 @@ public class OrderRepositoryV3 {
                 throw new IllegalStateException("예외 발생!");
             }
 
+            sleep(1000);
+
             trace.end(status);
         } catch (Exception e) {
             trace.exception(status, e);
             throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 }
