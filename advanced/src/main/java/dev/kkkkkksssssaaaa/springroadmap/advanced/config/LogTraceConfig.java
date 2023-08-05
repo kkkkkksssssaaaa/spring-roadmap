c0b2ad118a5d790875cc41ae2b81cefb4aaa0998
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-05 15:25:20 +0900

    feat: registered bean log trace

---
+++ advanced/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advanced/config/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package dev.kkkkkksssssaaaa.springroadmap.advanced.config;
+
+import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.logtarce.FieldLogTrace;
+import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.logtarce.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
