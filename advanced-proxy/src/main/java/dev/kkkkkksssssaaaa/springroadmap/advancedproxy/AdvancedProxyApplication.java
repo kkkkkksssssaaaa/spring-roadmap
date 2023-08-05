cae862a21a8eae931042c91f1bf69f1871b0efe2
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-05 22:08:28 +0900

    feat: add spring configuration annotations main class

--- advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/AdvancedProxyApplication.java
+++ advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/AdvancedProxyApplication.java
@@ -1,9 +1,12 @@
 package dev.kkkkkksssssaaaa.springroadmap.advancedproxy;
 
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "dev.kkkkkksssssaaaa.springroadmap.advancedproxy")
 public class AdvancedProxyApplication {
 
 	public static void main(String[] args) {
