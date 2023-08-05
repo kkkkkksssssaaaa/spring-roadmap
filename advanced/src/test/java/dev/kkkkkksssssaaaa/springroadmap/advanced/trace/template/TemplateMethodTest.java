9d40a7ed4a74c83f9e3adf337c267af0125c46a5
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-05 17:57:52 +0900

    test: add template method

---
+++ advanced/src/test/java/dev/kkkkkksssssaaaa/springroadmap/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,54 @@
+package dev.kkkkkksssssaaaa.springroadmap.advanced.trace.template;
+
+import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.template.code.AbstractTemplate;
+import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.template.code.SubClassLogic1;
+import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+
+        template2.execute();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        // start business logic
+        log.info("비지니스 로직1 실행");
+
+        long endTime = System.currentTimeMillis();
+
+        long result = endTime - startTime;
+
+        log.info("resultTime={}", result);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+
+        // start business logic
+        log.info("비지니스 로직2 실행");
+
+        long endTime = System.currentTimeMillis();
+
+        long result = endTime - startTime;
+
+        log.info("resultTime={}", result);
+    }
+}
