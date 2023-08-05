5905c8880cbf554a5dfd2e7855c1b7d40a3cbd7a
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-05 17:29:31 +0900

    refactor: change bean implementation type log trace

--- advanced/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advanced/config/LogTraceConfig.java
+++ advanced/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advanced/config/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package dev.kkkkkksssssaaaa.springroadmap.advanced.config;
 
-import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.logtarce.FieldLogTrace;
 import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.logtarce.LogTrace;
+import dev.kkkkkksssssaaaa.springroadmap.advanced.trace.logtarce.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
