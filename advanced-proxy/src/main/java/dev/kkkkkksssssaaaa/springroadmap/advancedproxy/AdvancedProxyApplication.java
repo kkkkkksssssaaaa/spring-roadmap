768875c00672943653cccffe81444089b25dae55
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-06 19:00:02 +0900

    feat: add component scan exclude filter to @Controller

--- advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/AdvancedProxyApplication.java
+++ advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/AdvancedProxyApplication.java
@@ -1,12 +1,27 @@
 package dev.kkkkkksssssaaaa.springroadmap.advancedproxy;
 
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.config.AppV1Config;
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
 
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "dev.kkkkkksssssaaaa.springroadmap.advancedproxy")
+@Import({
+	AppV1Config.class,
+	AppV2Config.class
+})
+@SpringBootApplication(
+	scanBasePackages = "dev.kkkkkksssssaaaa.springroadmap.advancedproxy"
+)
+@ComponentScan(
+	excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ANNOTATION,
+		classes = {Controller.class}
+	)
+)
 public class AdvancedProxyApplication {
 
 	public static void main(String[] args) {
