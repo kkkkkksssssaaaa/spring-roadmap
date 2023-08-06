3f26f58a497f8f9e156af223bd11cfba77130d2c
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-06 18:59:40 +0900

    fix: remove request mapping annotation

--- advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/v1/OrderControllerV1.java
+++ advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/v1/OrderControllerV1.java
@@ -2,13 +2,10 @@ package dev.kkkkkksssssaaaa.springroadmap.advancedproxy.v1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// 스프링은 @Controller 나 @RequestMapping 이 있어야 스프링 컨트롤러로 인식한다고 한다.
 @Controller
-@RequestMapping
 @ResponseBody
 public interface OrderControllerV1 {
 
