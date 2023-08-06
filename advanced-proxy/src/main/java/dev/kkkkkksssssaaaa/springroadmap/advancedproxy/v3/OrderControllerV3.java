6043be94f84c379652958040bf58499631a3cdae
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-06 19:54:22 +0900

    feat: add v3

---
+++ advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/v3/OrderControllerV3.java
@@ -0,0 +1,26 @@
+package dev.kkkkkksssssaaaa.springroadmap.advancedproxy.v3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 service;
+
+    public OrderControllerV3(OrderServiceV3 service) {
+        this.service = service;
+    }
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        service.orderItem(itemId);
+
+        return "OK";
+    }
+
+    @GetMapping("/v3/no-log")
+    public String noLog() {
+        return "OK";
+    }
+}
