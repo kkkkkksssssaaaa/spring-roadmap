510ac965297e06dba67fc56b66781d937f429283
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-12 23:35:36 +0900

    test: add proxy factory test case

--- advanced-proxy/src/test/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/proxy/proxyfactory/ProxyFactoryTest.java
+++ advanced-proxy/src/test/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,6 +1,7 @@
 package dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.proxyfactory;
 
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.advice.TimeAdvice;
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.service.ConcreteService;
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.service.ServiceImpl;
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
@@ -31,5 +32,44 @@ public class ProxyFactoryTest {
         assertTrue(AopUtils.isAopProxy(proxy));
         assertTrue(AopUtils.isJdkDynamicProxy(proxy));
         assertFalse(AopUtils.isCglibProxy(proxy));
+    }
+
+    @Test
+    void 구체_클래스만_있다면_CGLib_사용() {
+        ConcreteService service = new ConcreteService();
+
+        ProxyFactory proxyFactory = new ProxyFactory(service);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+
+        log.info("targetClass={}", service.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+
+        assertTrue(AopUtils.isAopProxy(proxy));
+        assertFalse(AopUtils.isJdkDynamicProxy(proxy));
+        assertTrue(AopUtils.isCglibProxy(proxy));
+    }
+
+    @Test
+    void proxyTargetClass옵션을_사용하면_인터페이스가_있어도_CGLib를_사용하고_클래스_기반_프록시를_사용() {
+        ServiceInterface service = new ServiceImpl();
+
+        ProxyFactory proxyFactory = new ProxyFactory(service);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass={}", service.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        assertTrue(AopUtils.isAopProxy(proxy));
+        assertFalse(AopUtils.isJdkDynamicProxy(proxy));
+        assertTrue(AopUtils.isCglibProxy(proxy));
     }
 }
