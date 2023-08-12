0418a3ced6e836dac7fe594c513dc07373e6c283
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-12 23:35:43 +0900

    test: add advisor test

---
+++ advanced-proxy/src/test/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,32 @@
+package dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.advisor;
+
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.advice.TimeAdvice;
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.service.ServiceImpl;
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.proxy.common.service.ServiceInterface;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+class AdvisorTest {
+
+    @Test
+    void test1() {
+        ServiceInterface target = new ServiceImpl();
+
+        ProxyFactory factory = new ProxyFactory(target);
+
+        // 가장 기본적인 어드바이저
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(
+            // 이 옵션을 주면 어느 대상이던간에 어드바이스를 적용한다는 뜻이 된다. == filter: *
+            Pointcut.TRUE,
+            new TimeAdvice()
+        );
+        factory.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface) factory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+}
