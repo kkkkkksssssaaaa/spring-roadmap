6d68ce7ca148ec94e1b049316cc90ab0b155a1eb
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-13 00:19:45 +0900

    feat: add proxy factory config

--- advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/AdvancedProxyApplication.java
+++ advanced-proxy/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedproxy/AdvancedProxyApplication.java
@@ -1,7 +1,7 @@
 package dev.kkkkkksssssaaaa.springroadmap.advancedproxy;
 
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.config.ExcludeComponent;
-import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.config.proxy.v1.dynamic.DynamicProxyFilterConfig;
+import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.config.proxy.v3.ProxyFactoryConfigV1;
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.trace.logtarce.LogTrace;
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.trace.logtarce.ThreadLocalLogTrace;
 import dev.kkkkkksssssaaaa.springroadmap.advancedproxy.v1.OrderControllerV1;
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 
-@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
 @SpringBootApplication(
 	scanBasePackages = "dev.kkkkkksssssaaaa.springroadmap.advancedproxy"
 )
