c0301146dd61766241d0a179430c0340106e7f63
kkkkkksssssaaaa <kofc312@naver.com>
2023-08-17 22:16:03 +0900

    feat: add aspect v2

--- advanced-aop/src/test/java/dev/kkkkkksssssaaaa/springroadmap/advancedaop/AopTest.java
+++ advanced-aop/src/test/java/dev/kkkkkksssssaaaa/springroadmap/advancedaop/AopTest.java
@@ -2,15 +2,18 @@ package dev.kkkkkksssssaaaa.springroadmap.advancedaop;
 
 import dev.kkkkkksssssaaaa.springroadmap.advancedaop.order.OrderRepository;
 import dev.kkkkkksssssaaaa.springroadmap.advancedaop.order.OrderService;
+import dev.kkkkkksssssaaaa.springroadmap.advancedaop.order.aop.AspectV2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
+@Import(AspectV2.class)
 @SpringBootTest
 public class AopTest {
 
