2a1a8523eb73b5ea43329ebd06635e9b31b775cc
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-03 00:44:17 +0900

    feat: add internal proxy test 2

---
+++ advanced-aop/src/main/java/dev/kkkkkksssssaaaa/springroadmap/advancedaop/internal/CallServiceV1.java
@@ -0,0 +1,28 @@
+package dev.kkkkkksssssaaaa.springroadmap.advancedaop.internal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    // Setter 주입을 사용하여 프록시 객체가 스프링 컨테이너에 등록 된 후에 해당 빈을 주입 받아 사용한다.
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("callServiceV1 setter={}", callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external() {
+        log.info("call external");
+        callServiceV1.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
