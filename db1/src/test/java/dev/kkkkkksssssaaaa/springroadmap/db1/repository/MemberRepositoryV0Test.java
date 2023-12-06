4ca01cf03977a15f9c29352fc8a14d8f23c0486c
kkkkkksssssaaaa <kofc312@naver.com>
2023-12-06 22:45:32 +0900

    test: add find by id test to member repository v0 test

--- db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/repository/MemberRepositoryV0Test.java
+++ db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,27 @@
 package dev.kkkkkksssssaaaa.springroadmap.db1.repository;
 
 import dev.kkkkkksssssaaaa.springroadmap.db1.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        // save
+        Member member = new Member("memberV1", 10000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertEquals(member, findMember);
     }
 }
