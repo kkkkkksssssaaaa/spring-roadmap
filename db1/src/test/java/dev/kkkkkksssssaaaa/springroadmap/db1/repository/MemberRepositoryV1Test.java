9a6140d5b6406a0b72c8b2071b70b20a8ba9e93d
kkkkkksssssaaaa <kofc312@naver.com>
2023-12-17 00:21:42 +0900

    test: add member repository v1 test

---
+++ db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,47 @@
+package dev.kkkkkksssssaaaa.springroadmap.db1.repository;
+
+import dev.kkkkkksssssaaaa.springroadmap.db1.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static dev.kkkkkksssssaaaa.springroadmap.db1.connection.ConnectionConst.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        // 기본 DriverManager - 항상 새로운 커넥션 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        // save
+        Member member = new Member("memberV3", 10000);
+        repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertEquals(member, findMember);
+
+        // update: money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertEquals(20000, updateMember.getMoney());
+
+        // delete
+        repository.delete(member.getMemberId());
+        assertThrows(NoSuchElementException.class, () -> repository.findById(member.getMemberId()));
+    }
+}
