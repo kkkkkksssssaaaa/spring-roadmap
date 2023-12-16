fd9b909e344a7fc55a471f1e914eaccc9543675d
kkkkkksssssaaaa <kofc312@naver.com>
2023-12-17 00:01:27 +0900

    test: add data source driver manager test

--- db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/connection/ConnectionTest.java
+++ db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/connection/ConnectionTest.java
@@ -2,7 +2,9 @@ package dev.kkkkkksssssaaaa.springroadmap.db1.connection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,6 +20,20 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection conn1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection conn2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        log.info("connection={}, class={}", conn1, conn1.getClass());
+        log.info("connection={}, class={}", conn2, conn2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection conn1 = dataSource.getConnection();
+        Connection conn2 = dataSource.getConnection();
 
         log.info("connection={}, class={}", conn1, conn1.getClass());
         log.info("connection={}, class={}", conn2, conn2.getClass());
