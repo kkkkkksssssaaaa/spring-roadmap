3a9f6ce8c1d3bab472ce32c348fc11f7b75d298d
kkkkkksssssaaaa <kofc312@naver.com>
2023-12-17 00:10:51 +0900

    test: add data source connection pool test

--- db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/connection/ConnectionTest.java
+++ db1/src/test/java/dev/kkkkkksssssaaaa/springroadmap/db1/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package dev.kkkkkksssssaaaa.springroadmap.db1.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,6 +30,21 @@ public class ConnectionTest {
     void dataSourceDriverManager() throws SQLException {
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws Exception {
+        // connection pooling
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
