5d760b04fa7b65680fd5c5f89973b3854060e371
kkkkkksssssaaaa <kofc312@naver.com>
2023-12-29 22:21:09 +0900

    feat: add member service v1

---
+++ db1/src/main/java/dev/kkkkkksssssaaaa/springroadmap/db1/service/MemberServiceV1.java
@@ -0,0 +1,30 @@
+package dev.kkkkkksssssaaaa.springroadmap.db1.service;
+
+import dev.kkkkkksssssaaaa.springroadmap.db1.domain.Member;
+import dev.kkkkkksssssaaaa.springroadmap.db1.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 repository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = repository.findById(fromId);
+        Member toMember = repository.findById(toId);
+
+        repository.update(fromId, fromMember.getMoney() - money);
+
+        validation(toMember);
+
+        repository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
