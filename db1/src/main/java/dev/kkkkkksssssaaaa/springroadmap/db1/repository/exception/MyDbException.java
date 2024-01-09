37411690ab710b7aa0053ab006c53c2a183be092
kkkkkksssssaaaa <kofc312@naver.com>
2024-01-09 23:37:57 +0900

    feat: add my db exception

---
+++ db1/src/main/java/dev/kkkkkksssssaaaa/springroadmap/db1/repository/exception/MyDbException.java
@@ -0,0 +1,19 @@
+package dev.kkkkkksssssaaaa.springroadmap.db1.repository.exception;
+
+public class MyDbException extends RuntimeException {
+
+    public MyDbException() {
+    }
+
+    public MyDbException(String message) {
+        super(message);
+    }
+
+    public MyDbException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDbException(Throwable cause) {
+        super(cause);
+    }
+}
