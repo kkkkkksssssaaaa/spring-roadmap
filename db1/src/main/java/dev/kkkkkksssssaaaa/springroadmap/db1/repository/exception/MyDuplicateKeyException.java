f0e497c6a8be0a732502e62a75880be5d164439c
kkkkkksssssaaaa <kofc312@naver.com>
2024-01-11 22:21:33 +0900

    feat: add my duplicate key exception class

---
+++ db1/src/main/java/dev/kkkkkksssssaaaa/springroadmap/db1/repository/exception/MyDuplicateKeyException.java
@@ -0,0 +1,19 @@
+package dev.kkkkkksssssaaaa.springroadmap.db1.repository.exception;
+
+public class MyDuplicateKeyException extends MyDbException {
+
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
